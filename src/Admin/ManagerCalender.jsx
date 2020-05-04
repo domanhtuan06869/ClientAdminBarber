496c513deff1beda9396201331276d6494127c76
TuanDoPt <domanh.tuanpt@gmail.com>
2020-05-04 17:39:28 +0700

    git update calender

--- src/Admin/ManagerCalender.jsx
+++ src/Admin/ManagerCalender.jsx
@@ -26,10 +26,12 @@ function ManagerCalender(props) {
   const [selectStore, setSelectStore] = useState('');
   const [calendarCut, setCalendarCut] = useState([]);
   const [find, setFind] = useState('')
+  const [listCalender, setListCalender] = useState([])
+  const [number, setNumber] = useState('1')
 
   const listStore = useSelector(state => state.reducerStore.data);
   const listCalendar = useSelector(state => state.reducerCalendar.data);
-  const filterCalendar = listCalendar.filter(item => item.phone_menber_cut.includes(find));
+
   const dispacth = useDispatch();
 
   useEffect(() => {
@@ -47,12 +49,9 @@ function ManagerCalender(props) {
   const handleChangeSelect = async text => {
     setLoading(true)
     setSelectStore(text.target.value)
-    const id = text.target.value
-    const { data } = await axios('/getmanagercalendarcrate?id_store=' + id);
-    dispacth({
-      type: 'FETCH_CALENDAR',
-      data: data
-    })
+    const name = text.target.value
+    const { data } = await axios(`https://api.tradenowvn.com/v1/other/haircut-getall?number=${number}&address=${name}`);
+    setListCalender(data.data)
     setLoading(false)
 
   }
@@ -62,16 +61,9 @@ function ManagerCalender(props) {
 
   const componentRef = useRef();
 
-  const styleCalendar = isReady => {
-    const style = { padding: 3, color: '#fff', backgroundColor: isReady ? '#009933' : '#ff9900', cursor: 'pointer' }
-    return style
-  }
   const updateCalendar = async (item) => {
     const { data } = await axios('/getmanagercalendarcrate?id_store=' + item.id_store)
-    dispacth({
-      type: 'FETCH_CALENDAR',
-      data: data
-    })
+    setListCalender(data)
 
   }
 
@@ -84,6 +76,20 @@ function ManagerCalender(props) {
       updateCalendar(item)
     })
 
+  }
+
+  const confirmUpdate = async (id) => {
+    let isCheck = window.confirm("Bạn có muốn lấp lịch cắt")
+    if (isCheck) {
+      callApi('get', 'https://api.tradenowvn.com/v1/other/haircut-order?id=' + id, {
+      }).then(async (res) => {
+        setLoading(true)
+        const { data } = await axios(`https://api.tradenowvn.com/v1/other/haircut-getall?number=${number}&address=${selectStore}`);
+        setListCalender(data.data)
+        setLoading(false)
+      })
+    } else {
+    }
   }
 
   return (
@@ -102,20 +108,16 @@ function ManagerCalender(props) {
           <select value={selectStore} onChange={handleChangeSelect} className="custom-select mr-sm-2" id="SelectAdrress">
             <option selected>Chọn một chi nhánh</option>
             {listStore.map((item) => (
-              <option key={item._id} value={item._id}>{item.address}</option>
+              <option key={item._id} value={item.addressLocation}>{item.addressLocation}</option>
             ))}
           </select>
         </div>
         <div style={{ marginTop: 26 }} className="col-lg-1.6">
-          <button className="btn btn-info mt-20">Hôm nay</button>
+          <button onClick={() => setNumber('1')} className="btn btn-info mt-20">Hôm nay</button>
         </div>
         <div style={{ marginTop: 26, marginLeft: 5 }} className="col-lg-1.6">
-          <button className="btn btn-info mt-20">Ngày mai</button>
+          <button onClick={() => setNumber('2')} className="btn btn-info mt-20">Ngày mai</button>
         </div>
-        <div style={{ marginTop: 26, marginLeft: 5 }} className="col-lg-1.6">
-          <button className="btn btn-info mt-20">Ngày kia</button>
-        </div>
-
       </div>
       <div className="row">
         {listCalendar.length > 0 ? <div class="col-lg-3">
@@ -134,82 +136,12 @@ function ManagerCalender(props) {
       </div>
       <div>
 
-        <div style={{marginTop:20}} className="row">
-          <div className="col-lg-1 border border-success">
-            8h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            8h30
-          </div>
-          <div className="col-lg-1 border border-success">
-            9h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            9h30
-          </div>
-          <div className="col-lg-1 border border-success">
-            10h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            10h30
-          </div>
-          <div className="col-lg-1 border border-success">
-           11h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            11h30
-          </div>
-          <div className="col-lg-1 border border-success">
-          12h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            12h30
-          </div>
-          <div className="col-lg-1 border border-success">
-            13h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            13h30
-          </div>
-          <div className="col-lg-1 border border-success">
-          14h
-          </div>
-          <div className="col-lg-1 border border-danger">
-          14h30
-          </div>
-          <div className="col-lg-1 border border-success">
-          15h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            15h30
-          </div>
-          <div className="col-lg-1 border border-success">
-           16h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            16h30
-          </div>
-          <div className="col-lg-1 border border-success">
-            17h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            17h30
-          </div>
-          <div className="col-lg-1 border border-success">
-            18h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            18h30
-          </div>
-          <div className="col-lg-1 border border-success">
-          19h
-          </div>
-          <div className="col-lg-1 border border-danger">
-            19h30
-          </div>
-          <div className="col-lg-1 border border-success">
-           20h
-          </div>
+        <div style={{ marginTop: 20 }} className="row">
+          {listCalender.map((item) =>
+            <div onClick={() => confirmUpdate(item.id)} className={`col-lg-1 border border-${item.exist ? 'success' : 'danger'}`}>
+              {item.time}
+            </div>
+          )}
         </div>
 
 
